eabe6aff0baf271407d702486d2898c8ead9ca9e
Ghost-Mechanic <158665405+Ghost-Mechanic@users.noreply.github.com>
2024-08-21 17:50:24 -0500

    Made buttons for removing items from cart and changing quantity functional in cart page

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,27 @@
 import '../styles/cart-item.css';
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import { useOutletContext } from 'react-router-dom';
 
-function CartItem({ item, handleDelete }) {
+function CartItem({ item }) {
+    const [numItems, setNumItems] = useState(item.quantity);
+    const [cart, setCart] = useOutletContext();
+
+    // change the cart with the new quantity each time numItems is changed
+    useEffect(() => {
+        let newNumItems = Number(numItems);
+
+        const newCart = cart.map((cartItem) => 
+            cartItem[0].id === item[0].id ? {...cartItem, quantity: newNumItems} : cartItem);
+
+        if (newNumItems === 0) {
+            setCart(cart.filter((cartItem) => cartItem[0].id !== item[0].id));
+        }
+        else {
+            setCart(newCart);
+        }
+    }, [numItems, cart, item, setCart]);
+
     return (
         <>
             <div className="cart-item-container">
@@ -9,11 +29,11 @@ function CartItem({ item, handleDelete }) {
                 <img className="cart-item-image" src={item[0].image} alt="" />
                 <h3 className="cart-item-price">${Number(item[0].price).toFixed(2)}</h3>
                 <div className="quantity">
-                    <button className="cart-item-button">-</button>
-                    <input type="number" value={item.quantity} min="0" max="10" />
-                    <button className="cart-item-button">+</button>
+                    <button className="cart-item-button" onClick={() => numItems > 0 && numItems <= 10 ? setNumItems(Number(numItems) - 1) : null}>-</button>
+                    <input type="number" value={numItems} min="0" max="10" disabled="true" />
+                    <button className="cart-item-button" onClick={() => numItems < 10 && numItems >= 0 ? setNumItems(Number(numItems) + 1) : null}>+</button>
                 </div>
-                <button className="remove-button" onClick={() => handleDelete(item[0].id)}>Remove from cart</button>
+                <button className="remove-button" onClick={() => setNumItems(0)}>Remove from cart</button>
             </div>
         </>
     )
@@ -21,7 +41,6 @@ function CartItem({ item, handleDelete }) {
 
 CartItem.propTypes = {
     item: PropTypes.array,
-    handleDelete: PropTypes.func,
 };
 
 export default CartItem;
--- src/components/CartMain.jsx
+++ src/components/CartMain.jsx
@@ -3,19 +3,13 @@ import CartItem from "./CartItem";
 import '../styles/cart-main.css';
 
 function CartMain() {
-    const [cart, setCart] = useOutletContext();
+    const [cart] = useOutletContext();
 
     cart.map((item) => {
         console.log(item[0].id);
         console.log(item[0].title);
         console.log(item.quantity);
     });
-
-    // this function handles items being deleted from the cart
-    function handleDeleteFromCart(id) {
-        const newCart = cart.filter((item) => item[0].id !== id);
-        setCart(newCart);
-    }
 
     return (
         <>
@@ -27,7 +21,7 @@ function CartMain() {
                         <h1>Looks like your cart is empty!</h1>
                         <h2>Visit our shopping page to find any item you are looking for!</h2>
                     </div>
-                    : cart.map((item) => <CartItem key={item[0].id} item={item} handleDelete={handleDeleteFromCart} />)}
+                    : cart.map((item) => <CartItem key={item[0].id} item={item} />)}
                 </div>
             </main>
         </>
