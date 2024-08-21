21f40c47a45ab7f9520da1129e1dfa004d85a3e4
Ghost-Mechanic <158665405+Ghost-Mechanic@users.noreply.github.com>
2024-08-21 00:35:35 -0500

    Fetched data from fakestore API to fill shop with items

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -23,15 +23,17 @@ function ItemCard({ item }) {
 
     return (
         <div className="item-card" tabIndex="0">
-            <h1 className="card-title">Mens Casual Premium Slim Fit T-Shirts </h1>
-            <img className="item-image" src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="" />
-            <h3 className="price">$22.30</h3>
-            <div className="add">
-                <button className="item-button" onClick={() => numItems > 0 && numItems <= 10 ? setNumItems(Number(numItems) - 1) : null}>-</button>
-                <input type="number" value={numItems} min="0" max="10" onChange={(e) => handleChangeNumItems(e)} onBlur={handleBlur} />
-                <button className="item-button" onClick={() => numItems < 10 && numItems >= 0 ? setNumItems(Number(numItems) + 1) : null}>+</button>
+            <h1 className="card-title">{item.title}</h1>
+            <img className="item-image" src={item.image} alt="" />
+            <h3 className="price">${Number(item.price).toFixed(2)}</h3>
+            <div className="add-container">
+                <div className="add">
+                    <button className="item-button" onClick={() => numItems > 0 && numItems <= 10 ? setNumItems(Number(numItems) - 1) : null}>-</button>
+                    <input type="number" value={numItems} min="0" max="10" onChange={(e) => handleChangeNumItems(e)} onBlur={handleBlur} />
+                    <button className="item-button" onClick={() => numItems < 10 && numItems >= 0 ? setNumItems(Number(numItems) + 1) : null}>+</button>
+                </div>
+                <button className="add-button">Add to cart</button>
             </div>
-            <button className="add-button">Add to cart</button>
         </div>
     )
 }
--- src/components/ShopMain.jsx
+++ src/components/ShopMain.jsx
@@ -1,28 +1,30 @@
 import ItemCard from "./ItemCard";
 import '../styles/shop-main.css';
+import { useState, useEffect } from "react";
 
 function ShopMain() {
+    const [loading, setLoading] = useState(true);
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=> {
+                console.log(json);
+                setProducts(json);
+            })
+            .catch((error) => console.log(error));
+
+        setLoading(false);
+    }, []);
+
     return (
         <>
             <main>
                 <h1 className="shop-title">Shop</h1>
                 <div className="items-container">
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
-                    <ItemCard />
+                    {!loading && products ? 
+                    products.map((item) => <ItemCard key={item.id} item={item} />) : null}
                 </div>
             </main>
         </>
